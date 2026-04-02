5f96a020ee9d9bd6eb933e412b2ccfaedd719146
Michael <milo0721@gmail.com>
2026-04-02 16:12:02 -0600

    updates

---
+++ migrations/013_drop_unique_constraint_from_name.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.alterTable('users', function (table) {
+    table.dropUnique('name');
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.alterTable('users', function (table) {
+    table.unique('name');
+  });
+};
